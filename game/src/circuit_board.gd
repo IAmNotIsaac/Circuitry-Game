a682d77d958681929d55c3cb7a23906f3d687934
Isaac Astell <isaacjulianastell@gmail.com>
2022-02-19 11:08:07 -0800

    removed debug code

--- game/src/circuit_board.gd
+++ game/src/circuit_board.gd
@@ -5,15 +5,6 @@ var size := Vector2.ONE
 var components : ComponentArray = ComponentArray.new()
 
 
-func _ready() -> void:
-	components.append(ComponentData.new())
-	components.append(ComponentData.new())
-	components.append(ComponentData.new())
-	
-	for component in components:
-		print(component)
-
-
 func add_component(comp_data : ComponentData, comp_position : Vector2) -> void:
 	components.append(comp_data)
 
@@ -23,7 +14,5 @@ func remove_component(comp_data : ComponentData) -> void:
 
 
 func get_component(comp_position : Vector2) -> Object:
-	for component in components:
-		pass
-	
+	# TODO: make method work
 	return Node2D.new()
